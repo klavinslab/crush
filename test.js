e2b249371ebbb7dd252d9e15d0def8742bc68901
Eric Klavins <klavins@uw.edu>
2022-01-30 00:09:29 +0000

    test connection

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
+const AQ = require("./gofish/aquarium.js")
+const fs = require('fs');
+
 function test() {
 
   console.log("Testing connection ...");
+
+  let config = JSON.parse(fs.readFileSync('config.json'));
+  AQ.config.aquarium_url = config.aquarium_url;
+  AQ.login(config.username, config.password)
+    .then(console.log);
 
 }
 
